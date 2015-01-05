8b7904502a4141d5ea213e08141673924cd54aad
Fabian Nack <fabiannack@web.de>
2015-01-05 01:35:36 +0100

    Initial beeper commit

---
+++ boards/msbiot/drivers/beeper.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     board_msbiot
+ * @{
+ *
+ * @file
+ * @brief       Beeper functionality implementation
+ *
+ * @author      Fabian Nack <nack@inf.fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "periph_conf.h"
+#include "periph/pwm.h"
+
+void msbiot_beeper_init(void)
+{
+}
---
+++ boards/msbiot/drivers/include/beeper.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "periph/gpio.h"
+#include "periph/pwm.h"
+
+#ifndef BEEPER_H_
+#define BEEPER_H_
+
+
+
+#endif /* BEEPER_H_ */
